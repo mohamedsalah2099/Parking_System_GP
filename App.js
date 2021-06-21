118ab677fb94791065956009721d1f90cc86cd71
1feby <feby.gergis97@eng-st.cu.edu.eg>
2021-06-21 10:51:44 +0200

    add sign in page

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { LinearGradient } from 'expo-linear-gradient';
+import SignIn from './components/signIn.js';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <LinearGradient style={styles.background} colors={['#0f4c5c', 'transparent']} />
+     <SignIn/>
     </View>
   );
 }
@@ -14,8 +14,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor:'#0d1b2a',
     alignItems: 'center',
     justifyContent: 'center',
+  },  background: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: 250,
   },
 });

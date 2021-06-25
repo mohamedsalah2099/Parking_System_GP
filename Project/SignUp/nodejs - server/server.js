5ed459a08b8910dd12545f3be0e2e46d365be3f5
Mohamed Salah <mohamedsalah2099@gmail.com>
2021-06-25 22:15:17 +0200

    added login route to server

--- Project/SignUp/nodejs - server/server.js
+++ Project/SignUp/nodejs - server/server.js
@@ -3,16 +3,20 @@ const app = express ();
 const port = 3000;
 const fetch = require ('node-fetch');
 var bodyParser = require ('body-parser');
-var cors = require('cors')
+var cors = require ('cors');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors())
+app.use (bodyParser.urlencoded ({extended: true}));
+app.use (bodyParser.json ());
+app.use (cors ());
 
-app.post ('/', async(req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    console.log(req.body);
+// Sign-Up Route ///
+app.post ('/', async (req, res) => {
+  res.header ('Access-Control-Allow-Origin', '*');
+  res.header (
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  console.log (req.body);
   try {
     await fetch ('https://beta.masterofthings.com/PostAppData', {
       method: 'post',
@@ -40,6 +44,48 @@ app.post ('/', async(req, res) => {
   res.send ('Hello World!');
 });
 
+// Login Route //
+app.post ('/login', async (req, res) => {
+  res.header ('Access-Control-Allow-Origin', '*');
+  res.header (
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  try {
+    await fetch ('https://beta.masterofthings.com/GetAppReadingValueList', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+      body: JSON.stringify ({
+        AppId: 43,
+        Limit: 10,
+        ConditionList: [
+          {
+            Reading: 'password',
+            Condition: 'e',
+            Value: '123456',
+          },
+          {
+            Reading: 'name',
+            Condition: 'e',
+            Value: 'Salah',
+          },
+        ],
+        Auth: {
+          Key: 'JKkjZahTZ8Crw2dk1624335681771users',
+        },
+      }),
+    })
+      .then (res => res.json())
+      .then (value => console.log (value.Info.NumberOfReadings));
+  } catch (e) {
+    console.log (e);
+  }
+
+  res.end ();
+});
+
 app.listen (port, () => {
-  console.log (`Example app listening at http://localhost:${port}`);
+  console.log (`Listening at http://localhost:${port}`);
 });
